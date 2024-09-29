78efcd4c60c16b2a6c58f88fa3085f7e944fbb39
vataspet <vataspet@fel.cvut.cz>
2024-09-29 21:42:04 +0200

    Adds first homework and lab

--- cv01.jl
+++ cv01.jl
@@ -1,1 +1,30 @@
-println("Hello, World.")
+#println("Hello, World.")
+
+"""
+    polynomial(a, x)
+
+Returns value of a polynomial with coefficients `a` at point `x`.
+"""
+function polynomial(a, x)
+    if eltype(a) != Char
+        accumulator = map(((v, w),) -> w * (x ^ (v-1)), enumerate(a))
+        return sum(accumulator)
+    else
+        ArgumentError("Incorrect element type in the argument 'a':\n$(a)")
+    end
+end
+
+a = [-19, 7, -4, 6] # list coefficients a from a^0 to a^n
+x = 3               # point of evaluation
+
+#println(polynomial(a, x))
+
+function addone(x)
+    return x + 1
+end
+
+af = [-19.0, 7.0, -4.0, 6.0]
+at = (-19, 7, -4, 6)
+ant = (a₀ = -19, a₁ = 7, a₂ = -4, a₃ = 6)
+a2d = [-19 -4; 7 6]
+ac = [2i^2 + 1 for i in -2:1]
---
+++ hw.jl
@@ -0,0 +1,29 @@
+using LinearAlgebra
+"""
+    polynomial(a, x)
+
+Returns value of a polynomial with coefficients `a` at point `x`. Taken from the first lesson.
+"""
+function polynomial(a, x)
+    if eltype(a) != Char
+        accumulator = map(((v, w),) -> w * (x ^ (v-1)), enumerate(a))
+        return sum(accumulator)
+    else
+        ArgumentError("Incorrect element type in the argument 'a':\n$(a)")
+    end
+end
+"""
+    circlemat(n)
+
+Creates an nxn matrix
+"""
+function circlemat(n)
+    return [(i == j - 1 && j > 1) || 
+            (i == n && j == 1) || 
+            (i == j + 1 && j < n) || 
+            (i == 1 && j == n) ? 1 : 0 
+            for i in 1:n, j in 1:n]
+end
+
+A = circlemat(10)
+return I + A + A^2 + A^3
